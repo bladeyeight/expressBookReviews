c9ed76d1cae1013bc587b942f63c55d37d488ac5
bladeyeight <samueljperry1991@gmail.com>
2024-02-07 15:12:17 -0500

    added put post and delete functionality for book reviews

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -45,9 +45,24 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = parseInt(req.params.isbn)
+    const selectedBook = books[isbn + ''];
+    const {review} = req.query;
+    const currentUser = req.session.authorization.username;
+    console.log( 'Query:', req.query, 'Review:', review, 'SelectedBook:', selectedBook, 'CurrentUser:', currentUser)
+    selectedBook.reviews[currentUser] = review;
+    return res.status(200).json(books[isbn + ''].reviews);
 });
+// delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = parseInt(req.params.isbn)
+    const selectedBook = books[isbn + ''];
+    const currentUser = req.session.authorization.username;
+    delete selectedBook.reviews[currentUser];
+    return res.status(200).json(books[isbn + ''].reviews);
+});
+
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
